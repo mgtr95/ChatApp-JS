8a52fbd6e57c8768de01c52d2d9808576bf1e057
mgtr95 <matijagudic@hotmail.com>
2023-03-14 15:33:10 +0100

    working v1

---
+++ scripts/getDOM.js
@@ -0,0 +1,41 @@
+export const DOM = {
+  membersCount: document.querySelector(".members-count"),
+  membersList: document.querySelector(".members-list"),
+  messages: document.querySelector(".messages"),
+  input: document.querySelector(".message-form__input"),
+  form: document.querySelector(".message-form"),
+};
+
+export function createMemberElement(member) {
+  const { name, color } = member.clientData;
+  const el = document.createElement("div");
+  el.appendChild(document.createTextNode(name));
+  el.className = "member";
+  el.style.color = color;
+  return el;
+}
+
+export function updateMembersDOM(members) {
+  DOM.membersCount.innerText = `${members.length} users in room:`;
+  DOM.membersList.innerHTML = "";
+  members.forEach((member) =>
+    DOM.membersList.appendChild(createMemberElement(member))
+  );
+}
+
+export function createMessageElement(text, member) {
+  const el = document.createElement("div");
+  el.appendChild(createMemberElement(member));
+  el.appendChild(document.createTextNode(text));
+  el.className = "message";
+  return el;
+}
+
+export function addMessageToListDOM(text, member) {
+  const el = DOM.messages;
+  const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+  el.appendChild(createMessageElement(text, member));
+  if (wasTop) {
+    el.scrollTop = el.scrollHeight - el.clientHeight;
+  }
+}
---
+++ scripts/index.js
@@ -0,0 +1,74 @@
+import { getRandomName, getRandomColor } from "./getRandom.js";
+import { updateMembersDOM, addMessageToListDOM, DOM } from "./getDOM.js";
+
+//declaring variables
+let members = [];
+
+//connect to channel
+const CHANNEL_ID = "PpOGDNqHIEJBEr4Z";
+const drone = new Scaledrone(CHANNEL_ID, {
+  data: {
+    // Will be sent out as clientData via events
+    name: getRandomName(),
+    color: getRandomColor(),
+  },
+});
+
+//connect to a room
+drone.on("open", (error) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log("Successfully connected to Scaledrone");
+
+  //joining room
+  const room = drone.subscribe("observable-mg_channel1");
+  room.on("open", (error) => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log("Successfully joined room");
+  });
+
+  //list of online members
+  room.on("members", (m) => {
+    members = m;
+    updateMembersDOM(members);
+  });
+
+  //user joined room
+  room.on("member_join", (member) => {
+    members.push(member);
+    updateMembersDOM(members);
+  });
+
+  //user left room
+  room.on("member_leave", ({ id }) => {
+    const index = members.findIndex((member) => member.id === id);
+    members.splice(index, 1);
+    updateMembersDOM(members);
+  });
+
+  //event listener for new messages
+  room.on("data", (text, member) => {
+    if (member) {
+      addMessageToListDOM(text, member);
+    } else {
+      // Message is from server
+    }
+  });
+
+  //sending message
+  DOM.form.addEventListener("submit", sendMessage);
+  function sendMessage() {
+    const value = DOM.input.value;
+    if (value === "") {
+      return;
+    }
+    DOM.input.value = "";
+    drone.publish({
+      room: "observable-mg_channel1",
+      message: value,
+    });
+  }
+});
